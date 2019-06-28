0c2627a2e57cbd78aceac5ff566306fedcc25843
Omri Ohayon <omri@rans-mbp-2.home>
2019-06-28 12:24:32 +0300

    initial commit...

---
+++ EasyDial/EasyDial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EasyDial
+//
+//  Created by Omri Ohayon on 28/06/2019.
+//  Copyright © 2019 Omri Ohayon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // initial commit
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
